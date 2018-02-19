e2d5e3ca9fa3f9dc50b1ce06a6a82e61c17e1bab
Reysmer Valle <reysmerwvr@gmail.com>
2018-02-19 14:29:28 -0400

    Albums Initial Commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    
+    //state = { albums: [] };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          albums: []
+        };
+    }
+    
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+    
+    render() {
+        return (
+            <ScrollView>
+                { this.renderAlbums() }
+            </ScrollView>
+        );
+    }
+}
+
+// Functional Component
+/* const AlbumList = () => {
+    return (
+        <View>
+            <Text>Album List!!!</Text>
+        </View>
+    );
+}; */
+
+export default AlbumList;
